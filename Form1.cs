c69acc99330f5f27766b9f810e496caff6a3bf1c
79817 <bazhenova.8@mail.ru>
2026-01-29 02:20:05 +0300

    Initial commit

---
+++ Form1.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace snow
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+    }
+}
