418528ab4e51c7c9a1e746b0969e7625e7046cd8
79817 <bazhenova.8@mail.ru>
2026-01-30 05:52:55 +0300

    добавила массив снежинок, реализовала анимацию,изменила название переменной desiredSize на randomSize

--- Form1.cs
+++ Form1.cs
@@ -6,29 +6,91 @@ namespace snow
 {
     public partial class Form1 : Form
     {
-        private int snowX = 100;
-        private int snowY = -20;
-        private int snowSpeed = 3;
+        private const int CountSnowFlakes = 150;
+        private const int TimerInterval = 50;
+
+        private SnowFlake[] snowflakes;
         private Bitmap snowflakeImage;
-        private Timer timer;
+        private System.Windows.Forms.Timer timer;
+
+        private static  Random rand = new Random();
+
+        private struct SnowFlake
+        {
+            public int X;
+            public int Y;
+            public int Speed;
+            public float Scale;
+        }
 
         public Form1()
         {
             InitializeComponent();
+
             snowflakeImage = Properties.Resources.snowflakeImg;
-            timer = new Timer();
-            timer.Interval = 20;
+            snowflakes = new SnowFlake[CountSnowFlakes];
+
+            //таймер
+            timer = new System.Windows.Forms.Timer();
+            timer.Interval = TimerInterval;
             timer.Tick += Timer_Tick;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            InitializeSnowflakes();
+            timer.Start();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            {
+                this.Close();
+            }
+        }
+
+        private void InitializeSnowflakes()
+        {
+            for (int i = 0; i < snowflakes.Length; i++)
+            {
+                ResetSnowFlake(i);
+            }
+        }
+
+        private void ResetSnowFlake(int index)
+        {
+            //размер снежинки
+            int randomSize = rand.Next(15, 51);
+
+            int speed;
+            if (randomSize <= 30)
+            {
+                speed = 2;
+            }
+            else
+            {
+                speed = 4;
+            }
+            float scale = (float)randomSize / snowflakeImage.Width;
+
+            snowflakes[index].X = rand.Next(ClientRectangle.Width);
+            snowflakes[index].Y = rand.Next(-200, 0); // появляется выше экрана
+            snowflakes[index].Speed = speed;
+            snowflakes[index].Scale = scale;
         }
 
         private void Timer_Tick(object sender, EventArgs e)
         {
-            snowY += snowSpeed;
-            if (snowY > ClientRectangle.Height)
+            for (int i = 0; i < snowflakes.Length; i++)
             {
-                snowY = -20;
-                snowX = new Random().Next(ClientRectangle.Width);
+                snowflakes[i].Y += snowflakes[i].Speed;
+
+                if (snowflakes[i].Y > ClientRectangle.Height)
+                {
+                    ResetSnowFlake(i);
+                }
             }
+
             Form1_Paint(this, new PaintEventArgs(CreateGraphics(), ClientRectangle));
         }
 
@@ -38,19 +100,17 @@ namespace snow
             using (var g = Graphics.FromImage(buffer))
             {
                 g.DrawImage(Properties.Resources.backgroundImg, ClientRectangle);
-                g.DrawImage(Properties.Resources.snowflakeImg, new Rectangle(snowX, snowY, 40, 40));
+                foreach (var flake in snowflakes)
+                {
+                    if (snowflakeImage != null)
+                    {
+                        int drawWidth = (int)(snowflakeImage.Width * flake.Scale);
+                        int drawHeight = (int)(snowflakeImage.Height * flake.Scale);
+                        g.DrawImage(snowflakeImage, flake.X, flake.Y, drawWidth, drawHeight);
+                    }
+                }
             }
             e.Graphics.DrawImage(buffer, ClientRectangle);
-        }
-
-        private void Form1_MouseDown(object sender, MouseEventArgs e)
-        {
-            timer.Start();
-        }
-
-        private void Form1_KeyDown(object sender, KeyEventArgs e)
-        {
-            this.Close();
         }
     }
 }
